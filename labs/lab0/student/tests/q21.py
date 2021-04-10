0a404e57388dfb58139ffcbb2a7117723085d79b
Your Name <you@example.com>
2021-04-10 07:22:11 +0000

    m

--- labs/lab0/student/tests/q21.py
+++
@@ -1,1 +0,0 @@
-test = {'name': 'q21', 'points': 3, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
--- labs/lab0/student/tests/q22.py
+++
@@ -1,1 +0,0 @@
-test = {'name': 'q22', 'points': 3, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}

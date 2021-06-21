ba31db4dc6a9b72558e3b641e6a5f8eddc2dada4
yidan sun <yidansun@ucsb.edu>
2021-06-20 21:53:20 -0700

    rm lab0 and lab1

--- labs/lab0/autograder/tests/q22.py
+++
@@ -1,7 +0,0 @@
-test = {   'name': 'q22',
-    'points': 3,
-    'suites': [   {   'cases': [{'code': ">>> 'a' in y22\nTrue", 'hidden': True, 'locked': False}, {'code': ">>> 'b' in y22\nTrue", 'hidden': True, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
